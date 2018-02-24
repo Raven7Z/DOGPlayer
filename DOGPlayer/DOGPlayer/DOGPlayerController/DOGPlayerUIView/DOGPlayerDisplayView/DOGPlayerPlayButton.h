e50ccd3938b05fbe7212b0018c0168ae44477265
butterfly <1142954060@qq.com>
2018-02-24 13:19:01 +0800

    【1.0.0】add control view play and pause button

---
+++ DOGPlayer/DOGPlayer/DOGPlayerController/DOGPlayerUIView/DOGPlayerDisplayView/DOGPlayerPlayButton.h
@@ -0,0 +1,13 @@
+//
+//  DOGPlayerPlayButton.h
+//  DOGPlayer
+//
+//  Created by butterfly on 24/2/18.
+//  Copyright © 2018年 RavenZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DOGPlayerPlayButton : UIButton
+
+@end
---
+++ DOGPlayer/DOGPlayer/DOGPlayerController/DOGPlayerUIView/DOGPlayerDisplayView/DOGPlayerPlayButton.m
@@ -0,0 +1,20 @@
+//
+//  DOGPlayerPlayButton.m
+//  DOGPlayer
+//
+//  Created by butterfly on 24/2/18.
+//  Copyright © 2018年 RavenZ. All rights reserved.
+//
+
+#import "DOGPlayerPlayButton.h"
+
+@implementation DOGPlayerPlayButton
+
++ (DOGPlayerPlayButton *)buttonWithType:(UIButtonType)buttonType {
+    DOGPlayerPlayButton *button = [super buttonWithType:buttonType];
+    
+
+    return button;
+}
+
+@end
